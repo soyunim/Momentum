8ceef2d0c1d4b1637b387001350e57d40f05d8a1
soyunim <togksk123@naver.com>
2023-08-28 00:11:20 +0900

    refactor:

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ function loginFormSubmit(event){
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
-
 }
 
 loginForm.addEventListener("submit",loginFormSubmit);
@@ -23,6 +26,5 @@ if(savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit",loginFormSubmit);
 }else {
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${savedUsername}`;
+    paintGreetings(savedUsername);
 }
