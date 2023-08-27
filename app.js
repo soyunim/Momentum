b6669b7777732eda1fe97aafa9b366ef22b6ba8a
soyunim <togksk123@naver.com>
2023-08-27 20:19:28 +0900

    refactor:

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const h1 = document.querySelector("#hello");
 const bg = document.body;
 
 function handleTitleClick(){
-    if(h1.style.color=="black" && bg.style.backgroundColor=="white"){
+    if(h1.style.color === "black" && bg.style.backgroundColor === "white"){
         h1.style.color="white";
         bg.style.backgroundColor="black";
         h1.innerHTML= "Black Mode!";
