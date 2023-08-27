7c7abaf85b9b1c8537f0eec55d04038148e3af5e
soyunim <togksk123@naver.com>
2023-08-28 00:10:08 +0900

    feat: load page with username

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function loginFormSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username",username);
+    localStorage.setItem(USERNAME_KEY, username);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 
 }
 
 loginForm.addEventListener("submit",loginFormSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",loginFormSubmit);
+}else {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${savedUsername}`;
+}
