d31e09579c3eef9196bfa54c118848b68fc65812
soyunim <togksk123@naver.com>
2023-08-28 00:01:21 +0900

    feat: get username

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function loginFormSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
 loginForm.addEventListener("submit",loginFormSubmit);
