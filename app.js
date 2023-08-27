df118a04bf5797c13643a1ec16833be84ca04850
soyunim <togksk123@naver.com>
2023-08-27 20:47:03 +0900

    refactor: using toggle()

--- app.js
+++ app.js
@@ -3,18 +3,8 @@ const h1 = document.querySelector("#hello");
 const bg = document.body;
 
 function handleTitleClick(){
-    const clickedClass ="clicked";
-
-    if(h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-        bg.classList.remove(clickedClass);
-        h1.innerHTML= "Light Mode!";
-    }
-    else{
-        h1.classList.add(clickedClass);
-        bg.classList.add(clickedClass);
-        h1.innerHTML= "Black Mode!";
-    }
+    h1.classList.toggle("clicked");
+    bg.classList.toggle("clicked");
 }
 
 btn.addEventListener("click", handleTitleClick);
