9cbe6faaa2cc6894c5a085ea7ff461d71dbc639f
soyunim <togksk123@naver.com>
2023-08-27 20:39:55 +0900

    refactor: change value name

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const h1 = document.querySelector("#hello");
 const bg = document.body;
 
 function handleTitleClick(){
-    if(h1.className === "active"){
+    const clickedClass ="clicked";
+
+    if(h1.className === clickedClass){
         h1.className = "";
         bg.className = "";
         h1.innerHTML= "Light Mode!";
     }
     else{
-        h1.className = "active";
-        bg.className = "active";
+        h1.className = clickedClass;
+        bg.className = clickedClass;
         h1.innerHTML= "Black Mode!";
     }
 }
