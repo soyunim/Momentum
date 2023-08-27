c4880d45818759085e27ee99f67c7ccf97788e9e
soyunim <togksk123@naver.com>
2023-08-27 20:35:04 +0900

    refactor: add css

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const h1 = document.querySelector("#hello");
 const bg = document.body;
 
 function handleTitleClick(){
-    if(h1.style.color === "black" && bg.style.backgroundColor === "white"){
-        h1.style.color="white";
-        bg.style.backgroundColor="black";
-        h1.innerHTML= "Black Mode!";
+    if(h1.className === "active"){
+        h1.className = "";
+        bg.className = "";
+        h1.innerHTML= "Light Mode!";
     }
     else{
-        h1.style.color="black";
-        bg.style.backgroundColor="white";
-        h1.innerHTML= "Light Mode!";
+        h1.className = "active";
+        bg.className = "active";
+        h1.innerHTML= "Black Mode!";
     }
 }
 
