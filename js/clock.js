a164c2c17ee7fa2f476f37bc7c40dad950188122
soyunim <togksk123@naver.com>
2023-08-28 16:21:34 +0900

    feat: Interval time

---
+++ js/clock.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector("#clock");
+
+function getTime(){
+    const date = new Date();
+    clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+setInterval(getTime,1000); //1초마다 getTime실행
