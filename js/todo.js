6e8e30daa56e359da368ed3aa47597b4bc428a80
soyunim <togksk123@naver.com>
2023-09-07 21:38:14 +0900

    feat: add new todo list

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    const newList = document.createElement("li");
+    const newSpan = document.createElement("span");
+    newList.appendChild(newSpan);
+    newSpan.innerHTML=newTodo;
+    toDoList.appendChild(newList);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
-    const newToDo = toDoInput.value;
+    const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
